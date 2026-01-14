d204e1ac9091e92e3f5b2272215c94f99d9aa76a
SleepySquash <nordnikita@icloud.com>
2026-01-14 15:24:35 +0500

    Try running only failing tests

--- test/e2e/configuration.dart
+++ test/e2e/configuration.dart
@@ -392,7 +392,7 @@ final FlutterTestConfiguration gherkinTestConfiguration =
         UsersParameter(),
         WidgetKeyParameter(),
       ]
-      ..tagExpression = 'not @disabled and not @internet'
+      ..tagExpression = 'not @disabled and @internet'
       // ..tagExpression = '@problem'
       ..createWorld = (config) => Future.sync(() => CustomWorld());
 
